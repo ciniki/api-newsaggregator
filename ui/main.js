77524c6838d113ce1c14f5c06d80524ec1632f11
Andrew Rivett <veggiefrog@gmail.com>
2020-05-18 13:41:27 -0400

    Updated to M.alert

--- ui/main.js
+++ ui/main.js
@@ -316,7 +316,7 @@ function ciniki_newsaggregator_main() {
         //
         var appContainer = M.createContainer(appPrefix, 'ciniki_newsaggregator_main', 'yes');
         if( appContainer == null ) {
-            alert('App Error');
+            M.alert('App Error');
             return false;
         }
     
